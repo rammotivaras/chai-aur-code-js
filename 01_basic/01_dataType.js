65117bee69c3c8dc40aee1753d2f2b7d9f7a5844
Ram Motivaras <rkmotivaras77@gmail.com>
2025-01-27 04:45:34 +0000

    dataType

---
+++ 01_basic/01_dataType.js
@@ -0,0 +1,24 @@
+"use strict" ; //treat all JS code as newer version 
+
+// alert (3 + 3) //we are usingnodejs, not browser 
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object 
+
+const name = 'ramji'
+const age = 18
+
+console.log(typeof 'ramji');
+console.log(typeof age);
+
+console.log(typeof null);  // object
+console.log(typeof undefined); //undefined
+
+
---
+++ 01_basic/veriables.js
@@ -0,0 +1,22 @@
+const accountId = 12345 ;
+let accountEmail = 'rkmotivaras@gmail.com' ;
+var accountPassword = '123123' ; 
+accountCity = 'Dwarka' ; 
+
+// accountId = 12 ; 
+
+accountEmail = 'hc@hc.com'
+accountPassword = "1239"
+accountCity = 'porbandar'
+let accountPrice ;
+
+ 
+console.log(accountId )
+
+/*
+prefer not to use var
+because of issue in block and functional scope
+*/
+
+
+console.table([accountEmail,accountId, accountPassword,accountCity,accountPrice])
