8f0af4f832d0a403cefa2349f797a58f4b675261
ramjimotivaras <ramji@21twelveinteractive.in>
2025-01-28 11:09:09 +0530

    datatype-summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,85 @@
+
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// let str = 'ramji' ; 
+// let num = 45 ;
+// let isLoggedIn = true ; 
+// let temp = null 
+// let state ;
+
+// let big = 3475481748175171847148714n ;
+// let sym = Symbol('123');
+
+
+// console.log(typeof str); 
+// console.log(typeof num);
+// console.log(typeof isLoggedIn);
+// console.log(typeof temp);
+// console.log(typeof state);
+// console.log(typeof big);
+// console.log(typeof sym);
+
+// string
+// number
+// boolean
+// object
+// undefined
+// bigint
+// symbol
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// ******Function ************ 
+
+// const value = function() {
+//     const nameIs = 'ramji';
+//     const age = 32;
+//     const gender = 'male';
+//     const city = 'dwarka';
+//     const contactNum = 8121182184;
+//     const education = 'graduate';
+  
+//     return {
+//       nameIs,
+//       age,
+//       gender,
+//       city,
+//       contactNum,
+//       education,
+//     };
+//   };
+  
+//   console.log(value());
+  
+
+// ****** Object ********
+// let Obj = {
+//     isLoggedIn : true,
+//     name : 'ramji',
+//     age : 32,
+
+// }
+// console.log(Obj); 
+
+
+// ****** Array ******  
+
+// let Heros = ['hanuman', 'shaktiman', 'mahaveer', 'kriss']
+
+// console.log(Heros[0]);
+
+
+
+
+
+
+
+
+
