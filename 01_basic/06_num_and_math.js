ffaa11bd6f1bdecb417dc8a76c3d4d413542a7c3
ramjimotivaras <ramji@21twelveinteractive.in>
2025-01-28 19:49:17 +0530

    math-num

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,55 @@
+
+
+const score = 400 ;
+// console.log(score);
+
+const balance = new Number(100) ;
+// console.log(balance);
+
+
+//  console.log(balance.toString().length); 3
+// console.log(balance.toFixed(2)); 100.00 
+
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3)); 23.9
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3)); 124
+
+
+const hundreds = 1000000; 
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ********** MATH ****************
+
+// console.log(Math);                  {}
+// console.log(Math.abs(-4));          4
+// console.log(Math.round(4.6));       5
+// console.log(Math.ceil(4.2));        5
+// console.log(Math.floor(5.9));       5 
+
+ 
+// console.log(Math.min(3,6,5,8,9));                     3
+// console.log(Math.max(9,7,68,5,454,3322,543,53,));   3322
+
+
+
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10  
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+ 
