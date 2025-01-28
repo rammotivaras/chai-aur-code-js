505cf6a0ff28781228fb2311d7cc7d05292ba375
ramjimotivaras <ramji@21twelveinteractive.in>
2025-01-28 09:48:26 +0530

    conversion of datatype

---
+++ 01_basic/01_conversionOperation.js
@@ -0,0 +1,40 @@
+
+// let score = '33abc' ;  
+
+// let score = null ; 
+let score = undefined ; 
+
+// console.log(typeof score);
+// console.log(typeof (score)); 
+ 
+let valueInNumber = Number(score) ; 
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// conversion 
+// '33' = 33
+// '33abc' = NaN 
+ // true = 1, 
+//  false = 0 ,
+
+
+let isLogedIn = 0 ;
+
+let booleanIsLogedIn = Boolean(isLogedIn);
+// console.log(booleanIsLogedIn);
+
+// 1 => true,
+// 0 => false,
+// '' = > false,
+// 'ramji' => true,
+
+
+
+let someNumber = 25
+ let stringNumber = String(someNumber) ; 
+  
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
