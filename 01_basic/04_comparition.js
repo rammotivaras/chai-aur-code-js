eea3a9891acc9ed8d2bb7caee22530a485d49d2c
ramjimotivaras <ramji@21twelveinteractive.in>
2025-01-28 10:30:16 +0530

    comparision

---
+++ 01_basic/04_comparition.js
@@ -0,0 +1,25 @@
+
+// console.log('2' > 0);     //  true
+// console.log('02' >= 0 );   // true
+
+
+// console.log(null > 0);   // false 
+// console.log(null >= 0);     //true
+// console.log(null == 0 );   //false
+
+
+// console.log(undefined == 0 );  false 
+// console.log(undefined > 0 );      false 
+// console.log(undefined >= 0 );      false 
+
+// console.log(undefined < 0);    false 
+
+
+
+
+
+
+
+
+
+
