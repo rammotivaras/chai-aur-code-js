7adbe7b62ad124401bb238c936ab3a91f0d54e6e
ramjimotivaras <ramji@21twelveinteractive.in>
2025-01-29 09:29:21 +0530

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,60 @@
+
+const myArray = [0,1,2,3,4,5] ; 
+const myArr2 = new Array(1,2,3,4,5) ; 
+
+// console.log(myArray);
+// console.log(myArr2[1]); 
+ 
+
+// Array Method 
+
+// myArray.push(6)
+// myArray.push(7)      add last one 
+// myArray.pop()    remove last one  
+// myArray.shift()       remove first one  
+// myArray.unshift(5)        add first one 
+
+
+// console.log(myArray.includes(9));    eather true or false value give 
+
+// console.log(myArray.includes(2));
+
+// console.log(myArray.indexOf(4));   if index number availible in your array then indexOf show the index number of arrays number if its not avalible in your array then its show -1 , means not avalible this number 
+
+
+
+const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);    convert in to a string 1,2,3,4,5
+
+
+
+
+// slice , splice 
+ 
+// console.log("A ", myArray);
+
+
+// const myn1 = myArray.slice(2,5)
+
+// console.log(myn1);
+// console.log("B ", myArray );
+
+
+// splice  => splice method change original array
+ 
+// console.log('A', myArray);
+
+// const myn2 = myArray.splice(1,4)
+// console.log(myn2); 
+// console.log('B', myArray);   0,5
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+
+const marvel_heros = ['ironman', 'thor', 'flash'] ; 
+
+const dc_heros = ['superman', 'spiderman', 'batman'] ; 
+
+// const all_heros = marvel_heros.concat(dc_heros) right but dont use this concat to merg 2 SafeArray
+// console.log(all_heros); 
+
+// use ...spred operator for merg array 
+
+// const all_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_heros);
+
+
+// const another_array = [1,2,3,[4,3,2,2],8,9,8,7,[2,32,4,5,5]]
+
+// const real_array = another_array.flat(Infinity)
+// console.log(real_array);
+
+
+// console.log(Array.isArray('ramji'));
+// console.log(Array.from('ramji'));
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3));
+
